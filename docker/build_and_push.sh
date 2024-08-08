c3adc7a8693bf1eb8902701f7250cac4be841eba
Mark Cooper <mark.c.cooper@outlook.com>
2024-08-08 14:59:28 -0700

    Support build push to ecr

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -3,3 +3,11 @@
 docker compose build
 docker tag docker-aspace-proxy lyrasis/aspace-proxy:latest
 docker push lyrasis/aspace-proxy:latest
+
+if [ -z "$ASPACE_PROXY_ECR_IMG" ] ; then
+  echo "ASPACE_PROXY_ECR_IMG not set, skipping push to ECR"
+  exit 0
+fi
+
+docker tag docker-aspace-proxy $ASPACE_PROXY_ECR_IMG
+docker push $ASPACE_PROXY_ECR_IMG
